731df0d0c408c852b4428df610fd4618394c242f
Jason Huang <jason97521@gmail.com>
2023-04-13 04:09:01 -0400

    video card component

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ const Main = styled.div`
   background-color: ${({ theme }) => theme.bg};
 `
 const Wrapper = styled.div`
-  
+  padding: 22px 96px;
 `
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(true)
+  const [darkMode, setDarkMode] = useState(false)
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
